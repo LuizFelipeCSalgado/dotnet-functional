eb685671937324338961774378ca87567e312c38
Luiz Felipe Salgado <luizfelipecsalgado@gmail.com>
2020-02-09 23:47:06 -0300

    [cap1] introdução

---
+++ cap1-hello-world-csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace cap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculadora = new Calculadora();
+            int soma = calculadora.SomaQuadradoDosNumerosAteDez();
+            Console.WriteLine("soma = {0}", soma);
+        }
+    }
+
+    class Calculadora
+    {
+        public int ElevaNumeroAoQuadrado(int numero)
+        {
+            return numero * numero;
+        }
+
+        // public int SomaQuadradoDosNumerosAteDez()
+        // {
+        //     int soma = 0;
+        //     for (int numero = 1; numero <= 10; numero++)
+        //         soma += ElevaNumeroAoQuadrado(numero);
+        //     return soma;
+        // }
+
+        public int SomaQuadradoDosNumerosAteDez()
+        {
+            return Enumerable.Range(1, 10)
+                             .Select(ElevaNumeroAoQuadrado)
+                             .Sum();
+        }
+    }
+}
